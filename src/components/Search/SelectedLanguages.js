cd92acd210cf93fbc4ff7d606f5e798df27d5bee
hsy <69149030+dev-redo@users.noreply.github.com>
2022-05-15 00:30:31 +0900

    Fix: 선택한 아이템 list 렌더 에러 해결
    선택한 아이템들을 렌더하는 SelectedLanguages 컴포넌트가  선택한 아이템들이 5개 미만일 때 제대로 렌더링 되지
    않는 문제를 해결했습니다.

--- src/components/Search/SelectedLanguages.js
+++ src/components/Search/SelectedLanguages.js
@@ -4,6 +4,7 @@ export default function SelectedLanguage({ $target, initialState }) {
   this.$element = document.createElement('div');
   this.$element.className = 'SelectedLanguage';
   this.state = initialState;
+  console.log(this.state);
 
   $target.appendChild(this.$element);
   this.setState = (nextState) => {
@@ -13,7 +14,10 @@ export default function SelectedLanguage({ $target, initialState }) {
 
   this.render = () => {
     const startPosition = this.state.length - MAX_DISPLAY_COUNT;
-    const items = this.state.slice(startPosition, MAX_DISPLAY_COUNT + startPosition);
+    const items =
+      startPosition >= 0
+        ? this.state.slice(startPosition, MAX_DISPLAY_COUNT + startPosition)
+        : this.state;
     this.$element.innerHTML = `
             <ul>
                 ${items.map((item) => `<li>${item}</li>`).join('')}
